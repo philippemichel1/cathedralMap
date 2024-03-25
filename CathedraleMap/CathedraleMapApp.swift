634083e22e0fee67fbc8facfd7ba5387dec374b1
Philippe MICHEL <philippe.michel1@gmail.com>
2024-03-25 12:41:16 +0100

    Initial Commit

---
+++ CathedraleMap/CathedraleMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  CathedraleMapApp.swift
+//  CathedraleMap
+//
+//  Created by Philippe MICHEL on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct CathedraleMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
